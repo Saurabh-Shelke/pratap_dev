d3225f0d04ab174171a61efd5d213ba3e6348116
Saurabh-Shelke <saurabhshelke28032002@gmail.com>
2026-04-01 00:39:33 +0530

    Change Opp

--- pratap_dev/public/js/opportunity_override.js
+++ pratap_dev/public/js/opportunity_override.js
@@ -1,12 +1,18 @@
-function set_opportunity_from_options(frm) {
-    frm.set_df_property("opportunity_from", "options", "\nCustomer\nProspect");
+function set_opportunity_from_filter(frm) {
+    frm.set_query("opportunity_from", () => {
+        return {
+            filters: {
+                name: ["in", ["Customer", "Prospect"]],
+            },
+        };
+    });
 }
 
 frappe.ui.form.on("Opportunity", {
     setup(frm) {
-        set_opportunity_from_options(frm);
+        set_opportunity_from_filter(frm);
     },
     refresh(frm) {
-        set_opportunity_from_options(frm);
+        set_opportunity_from_filter(frm);
     },
 });
